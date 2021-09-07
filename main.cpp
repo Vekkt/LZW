c2e6e2bd1d31e3f3f0dc405b5ed4925278dd1a99
vekkt <francois.burguet@epfl.ch>
2021-09-07 19:28:27 +0200

    added program arguments

--- main.cpp
+++ main.cpp
@@ -18,54 +18,79 @@ private: friend
     }
 };
 
+std::string get_raw_name(const std::string& s) {
+    return s.substr(0, s.find_last_of('.'));
+}
+
 double ratio(auto size1, auto size2) {
     return 100 * (1 - static_cast<double>(size1) / static_cast<double>(size2));
 }
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
-        std::cout << "Wrong number of arguments. Exiting." << std::endl;
+    if (argc == 1) {
+        std::cerr << "Wrong number of arguments. Exiting." << std::endl;
         return 1;
     }
 
-    std::string input_file_name{"./" + std::string{argv[1]}};
-    std::string compr_file_name{"./" + std::string{argv[2]}};
-    std::string output_file_name{"./" + std::string{argv[3]}};
+    std::string mode{argv[1]};
+    if (mode == "-c") {
+        std::string input_file_name{"./" + std::string{argv[2]}};
+        std::string compr_file_name{"./"};
 
-    try {
-        std::ifstream in{input_file_name};
-        std::ofstream out{compr_file_name};
+        if (argc == 4)
+            input_file_name += std::string{argv[3]};
+        else
+            compr_file_name += get_raw_name(input_file_name) + ".lzw";
 
-        std::cout << "Starting compression..." << std::endl;
-        auto start = std::chrono::high_resolution_clock::now();
-        LZW::compress(in, out);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "File compressed in: " << duration.count() / 1e6 << " seconds" << std::endl;
+        try {
+            std::ifstream in{input_file_name};
+            std::ofstream out{compr_file_name};
 
-        auto original_size = std::filesystem::file_size(input_file_name);
-        auto compress_size = std::filesystem::file_size(compr_file_name);
-        std::cout << "Original file size: " << SizeReader{original_size} << std::endl;
-        std::cout << "Compressed file size: " << SizeReader{compress_size} << std::endl;
-        std::cout << "Compression ratio: " << ratio(compress_size, original_size) << "%" << std::endl;
-    } catch(std::exception& e){
-        std::cout << "Error encountered during compression. Exiting." << std::endl;
+            if (!in.is_open() || !out.is_open()) {
+                std::cerr << "Could not open file. Exiting." << std::endl;
+                return 1;
+            }
+
+            std::cout << "Starting compression... ";
+            auto start = std::chrono::high_resolution_clock::now();
+            LZW::compress(in, out);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+            std::cout << "File compressed in " << duration.count() / 1e6 << " seconds" << std::endl;
+
+            auto original_size = std::filesystem::file_size(input_file_name);
+            auto compress_size = std::filesystem::file_size(compr_file_name);
+            std::cout << "Original file size: " << SizeReader{original_size} << std::endl;
+            std::cout << "Compressed file size: " << SizeReader{compress_size} << std::endl;
+            std::cout << "Compression ratio: " << ratio(compress_size, original_size) << "%" << std::endl;
+        } catch(std::exception& e){
+            std::cerr << "Error encountered during compression. Exiting." << std::endl;
+        }
+    } else if (mode == "-d") {
+        std::string compr_file_name{"./" + std::string{argv[2]}};
+        std::string output_file_name{"./"};
+
+        if (argc == 4)
+            output_file_name += std::string{argv[3]};
+        else
+            output_file_name += get_raw_name(compr_file_name) + ".txt";
+
+        try {
+            std::ifstream in{compr_file_name};
+            std::ofstream out{output_file_name};
+
+            std::cout << "Starting decompression... ";
+            auto start = std::chrono::high_resolution_clock::now();
+            LZW::decompress(in, out);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+            std::cout << "File decompressed in " << duration.count() / 1e6 << " seconds" << std::endl;
+        } catch(std::exception& e){
+            std::cerr << "Error encountered during decompression. Exiting." << std::endl;
+        }
+    } else {
+        std::cerr << "Unknown parameter. Must be one of [-c, -d]." << std::endl;
+        return 1;
     }
-
-    std::cout << "-----------------------" << std::endl;
-
-    try {
-        std::ifstream in{compr_file_name};
-        std::ofstream out{output_file_name};
-
-        auto start = std::chrono::high_resolution_clock::now();
-        LZW::decompress(in, out);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "File decompressed in: " << duration.count() / 1e6 << " seconds" << std::endl;
-    } catch(std::exception& e){
-        std::cout << "Error encountered during decompression. Exiting." << std::endl;
-    }
-
     return 0;
 }
